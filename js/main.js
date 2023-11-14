909d5da9a9e3f5d18e873af5647f909c883ca1d9
Sasi2294 <montella.salvatore@live.it>
2023-11-14 16:59:31 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
 // Questo è il file che contiene tutto il codice JS della pagina web
+function userFormComplete(event){
+    event.preventDefault();
 
-// PASSO 1: Chiedere  all'utente quanti km deve percorrere e l'età
-// PASSO 2: Calcolare il prezzo del viaggio in base alle informazioni inserite seguendo le regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// Output di prezzo con massimo 2 decimali.
+let userNameSurname = document.getElementById("credenziali").value;
+let userKm = document.getElementById("kmTravel").value;
+let userAge = document.getElementById("age").value;
 
 
-/*let userKm = prompt("Quanti kilometri devi percorrere?");
-let userAge = prompt("Inserisci la tua età");
 
-if (isNaN(userKm) || isNaN(userAge)) {
-    alert ("Non hai inserito i dati correttamente!")
+if (isNaN(userKm)) {
+    alert ("Alcuni dati inseriti sono errati, riprova!");
 }
 
-    userKm = parseInt(userKm);
-    userAge = parseInt(userAge);
+ userKm = parseInt(userKm);
 
-    let ticketPrice = userKm * 0.21; 
+ let ticketPrice = userKm * 0.21;
 
-if (userAge < 18) {
-    ticketPrice -= ticketPrice * 20 / 100; 
-} else if (userAge >= 65) {
-    ticketPrice -= ticketPrice * 40 / 100; 
+ if (userAge === "1") {
+    ticketPrice -= ticketPrice * 20 / 100;
+ } else if (userAge === "3") {
+    ticketPrice -= ticketPrice * 40 / 100;
+ }
+
+ let finalPrice = ticketPrice.toFixed(2);
+ let random = Math.floor(Math.random() * 11);
+ let random2 = Math.floor(Math.random() * 10001);
+
+ document.getElementById("name-surname").innerHTML = userNameSurname;
+ document.getElementById("trainCarriage").innerHTML = random;
+ document.getElementById("cpCode").innerHTML = random2;
+ document.getElementById("ticketPrice").innerHTML = finalPrice + "€";
 }
 
-let finalPrice = ticketPrice.toFixed(2);
-
-console.log("Il prezzo del tuo biglietto è " + finalPrice);
-document.getElementById("price").innerHTML = finalPrice;
     
